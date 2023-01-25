541ba5ca4ecb84f6d63c7aef980bc339b7acf0d3
charaf marghin <developermarghin@gmail.com>
2023-01-25 14:29:58 +0100

    add login functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,8 @@
+const Admin = require('../models/admin');
+const bcrypt = require('bcryptjs');
+const jwtChecker = require('../utils/jwtChecker');
+const jwtGenerator = require('../utils/jwtGenerator');
+
 /**
  * @route   POST api/auth
  * @desc    Authenticate admin and get token
@@ -5,7 +10,48 @@
  * @method  POST
  */
 const login = async (req, res, next) => {
-  // TODO: login controller
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      let error = new Error('Please enter all fields');
+      error.status = 400;
+      throw error;
+    }
+
+    // check if admin exists
+    const admin = await Admin.findOne({ email });
+    if (!admin) {
+      let error = new Error('Invalid credentials');
+      error.status = 400;
+      throw error;
+    }
+
+    // check if password matches
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) {
+      let error = new Error('Invalid credentials');
+      error.status = 400;
+      throw error;
+    }
+
+    // generate jwt token
+    const token = jwtGenerator({ _id: admin._id, email: admin.email });
+
+    // send token in cookie
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 1000 * 60 * 60 * 24 * 2, // 2 days
+    });
+
+    res.status(200).json({
+      success: true,
+      admin,
+    });
+  } catch (e) {
+    next(e);
+  }
 };
 
 /**
