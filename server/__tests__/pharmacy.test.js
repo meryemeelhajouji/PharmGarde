78e077a9400578e5a89134b86fbb2e13e476a428
meryemeelhajouji <meryemelhajouji.99@gmail.com>
2023-01-25 19:15:41 +0100

    add test unitaire de pharmacie de garde

--- server/__tests__/pharmacy.test.js
+++ server/__tests__/pharmacy.test.js
@@ -1,18 +1,16 @@
+const supertest = require('supertest');
+const app = require('../index');
 
-const supertest = require("supertest");
-const app = require("../index");
+describe('pharmacy', () => {
+  it('getGardingPharmacies ', async () => {
+    const res = await supertest(app).get('/api/pharmacy/gard/active');
+    expect(res.statusCode).toEqual(200);
+  });
 
-describe("pharmacy", () => {
-    it("getGardingPharmacies ", async () => {
-      const res = await supertest(app).get("/api/pharmacy/gard/active")
-      expect(res.statusCode).toEqual(200);
+  it('changePharmacyState ', async () => {
+    const res = await supertest(app).get('/api/pharmacy/gard/63d0fd993cdd15fc831391f5').send({
+      statuts: true,
     });
-
-    it("changePharmacyState ", async () => {
-        const res = await supertest(app).get("/api/pharmacy/gard/63d0fd993cdd15fc831391f5")   .send({
-            statuts: true,
-          });
-        expect(res.statusCode).toEqual(200);
-      });
-
-})
+    expect(res.statusCode).toEqual(200);
+  });
+});
