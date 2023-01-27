f1acc46965dd8edccae3ea80fb7120ca5a47997e
charaf marghin <developermarghin@gmail.com>
2023-01-27 15:16:14 +0100

    add error handler middlware

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const initDB = require('./config/initDB');
 const projectRoutes = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
 
 // initialize the database
 (async () => {
@@ -13,15 +14,18 @@ const projectRoutes = require('./routes');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 
 // routes
 app.use(projectRoutes);
+
+// error handler
+app.use(errorHandler);
 
 const port = 5000 || process.env.PORT;
 app.listen(port, () => {
   process.env.NODE_ENV != 'production' && console.log(`server running in development mode on port ${port}`);
 });
 
-module.exports = app
+module.exports = app;
