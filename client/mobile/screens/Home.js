0cbc60f3c80db6944b7e4fe88a6f8851087d849c
meryemeelhajouji <meryemelhajouji.99@gmail.com>
2023-02-06 09:01:37 +0100

    create NavigationContainer

---
+++ client/mobile/screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+const Home = () => {
+  return (
+    <View>
+      <Text> hello home</Text>
+    </View>
+  );
+};
+
+export default Home;
---
+++ client/mobile/screens/Pharmacie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+const Pharmacie = () => {
+  return (
+    <View>
+      <Text> hello Pharmacie</Text>
+    </View>
+  );
+};
+
+export default Pharmacie;
