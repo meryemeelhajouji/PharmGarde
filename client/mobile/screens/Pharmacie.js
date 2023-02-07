6e591098593b1e716fbfdd50a26ae5a8bcf647d3
fatimazhra123 <fatimazahrabouamoud@gmail.com>
2023-02-07 15:03:58 +0100

    terminez slide gide

--- client/mobile/screens/Pharmacie.js
+++ client/mobile/screens/Pharmacie.js
@@ -3,7 +3,9 @@ import { Text, View } from 'react-native';
 const Pharmacie = () => {
   return (
     <View>
-      <Text> hello Pharmacie</Text>
+      <Text> 
+        hello Pharmacie
+        </Text>
     </View>
   );
 };
