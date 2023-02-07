8d179bae05c4be3c80b487e3cb8f20881bb829bf
SafiaMaani <saf.maani@gmail.com>
2023-02-07 14:26:39 +0100

    Navbar

--- client/mobile/App.js
+++ client/mobile/App.js
@@ -1,19 +1,14 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Navigator from './navigation/Navigator';
 import Home from './screens/Home';
 import Pharmacie from './screens/Pharmacie';
 
-const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{ title: 'Welcome to Home' }} />
-        
-        <Stack.Screen name="Pharmacie" component={Pharmacie} options={{ title: 'Welcome to Pharmacie' }} />
-      </Stack.Navigator>
+      <Navigator />
     </NavigationContainer>
   );
 };
---
+++ client/mobile/navigation/Navigator.js
@@ -0,0 +1,37 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+import Map from '../screens/Map';
+import Pharmacie from '../screens/Pharmacie';
+import { MaterialIcons, Feather } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+const Navigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => <Feather name="home" size={size} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="map"
+        component={Map}
+        options={{
+          tabBarIcon: ({ color, size }) => <Feather name="map-pin" size={size} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="pharmacie"
+        component={Pharmacie}
+        options={{
+          tabBarIcon: ({ color, size }) => <MaterialIcons name="local-pharmacy" color={color} size={size} />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Navigator;
