5075a4ccdb7ac364975662cc2fdd152d9213a10f
charaf marghin <developermarghin@gmail.com>
2023-02-07 16:52:52 +0100

    add MapView component

--- client/mobile/screens/Map.js
+++ client/mobile/screens/Map.js
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import React from 'react';
 
 const Map = () => {
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
-  )
-}
+  );
+};
 
-export default Map
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default Map;
