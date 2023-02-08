9553f67877b7e4a73f6b0bdc88cbd88d6fc17df0
charaf marghin <developermarghin@gmail.com>
2023-02-08 08:45:06 +0100

    add region coordinates to the map

--- client/mobile/screens/Map.js
+++ client/mobile/screens/Map.js
@@ -1,11 +1,59 @@
-import { View, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
-import React from 'react';
+import { View, StyleSheet, Pressable, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useEffect, useState } from 'react';
 
 const Map = () => {
+  const [myRegion, setMyRegion] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  const [error, setError] = useState(null);
+
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      return setError('Permission to access location was denied');
+    }
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+    setMyRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map} region={myRegion} provider="google">
+        <Marker coordinate={myRegion} title="My Location" description="This is my location" />
+      </MapView>
+
+      <Pressable
+        style={{
+          position: 'absolute',
+          bottom: 20,
+          right: 20,
+          backgroundColor: 'white',
+          padding: 10,
+          borderRadius: 50,
+        }}
+        onPress={getLocation}
+      >
+        <MaterialIcons name="my-location" size={24} color="black" />
+      </Pressable>
+
+      {error && <Text>{error}</Text>}
     </View>
   );
 };
