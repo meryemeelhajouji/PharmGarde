ad37ff539dac374e50dcdbf28d371e399fd15345
charaf marghin <developermarghin@gmail.com>
2023-02-12 15:20:39 +0100

    update the naming of pages

--- client/dashboard/src/pages/index.js
+++ client/dashboard/src/pages/index.js
@@ -1,7 +1,7 @@
 // import AppartementsPage from "./AppartementsPage";
-import PharmaciesPage from './dashboard/PharmaciesPage';
-import HomePage from './dashboard/HomePage';
-import CommentairesPage from './dashboard/CommentairesPage';
+import PharmaciesPage from './dashboard/Pharmacies';
+import HomePage from './dashboard/Home';
+import CommentairesPage from './dashboard/Commentaires';
 import LoginPage from './Login';
 import ForgetPasswordPage from './ForgetPassword';
 import ResetPassword from './ResetPassword';
