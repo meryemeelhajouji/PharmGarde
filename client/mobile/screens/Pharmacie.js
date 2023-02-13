00aec5f156459f98edda0d64db645e86217201b0
charaf marghin <developermarghin@gmail.com>
2023-02-13 02:11:48 +0100

    add search functionality

--- client/mobile/screens/Pharmacie.js
+++ client/mobile/screens/Pharmacie.js
@@ -1,26 +1,34 @@
-import React from 'react';
 import { ScrollView, View } from 'react-native';
 import CardPharmacy from '../component/CardPharmacy';
-import { Appbar } from 'react-native-paper';
+import { Appbar, TextInput } from 'react-native-paper';
 import { useState, useEffect } from 'react';
 import { getAllPharmacies } from '../utils/api';
 
 const Pharmacie = () => {
   const [pharmacies, setPharmacies] = useState([]);
+  const [cachedPharmacies, setCachedPharmacies] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const fetchPharmacies = async () => {
       const data = await getAllPharmacies();
       setPharmacies(data.data);
+      setCachedPharmacies(data.data);
     };
     fetchPharmacies();
   }, []);
 
+  const handleChange = (text) => {
+    setSearch(text);
+    const filtered = cachedPharmacies.filter((pharmacy) => pharmacy.name.toLowerCase().includes(text.toLowerCase()));
+    setPharmacies(filtered);
+  };
+
   return (
     <View>
       <Appbar.Header>
-        <Appbar.Content title="All Pharmacies" subtitle={'Subtitle'} />
         <Appbar.Action icon="magnify" onPress={() => {}} />
+        <TextInput value={search} style={{ width: '100%' }} onChangeText={handleChange} placeholder="Search" />
       </Appbar.Header>
       <ScrollView
         contentContainerStyle={{
